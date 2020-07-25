87610907f0a7557a60c6f1b874452258ea6db4f7
Diogo Lindoso <diogolindoso@gmail.com>
2020-07-25 15:03:56 -0400

    removido prefixo nome

--- database/factories/DocumentoFactory.php
+++ database/factories/DocumentoFactory.php
@@ -9,28 +9,29 @@ use Faker\Provider\Base;
 
 $factory->define(Documento::class, function (Faker $faker) {
 
-    $estadoCivil = ['Solteiro(a)','Casado(a)','Divorciado(a)','Viuvo(a)','União Estável','Outros'];
-    $tipoDocumento = ['Civil','Militar','Profissional'];
-    $rand = rand(0,1);
-    $sexo = [0 => ['M','male'], 1 => ['F','female']];
-    $idmunicipios = App\Estado::where('nome','Amazonas')->get()->first()->municipios->modelKeys();
+    $estadoCivil = ['Solteiro(a)', 'Casado(a)', 'Divorciado(a)', 'Viuvo(a)', 'União Estável', 'Outros'];
+    $tipoDocumento = ['Civil', 'Militar', 'Profissional'];
+    $rand = rand(0, 1);
+    $sexo = [0 => ['M', 'male'], 1 => ['F', 'female']];
+    $idmunicipios = App\Estado::where('nome', 'Amazonas')->get()->first()->municipios->modelKeys();
     $municipio = App\Municipio::find(Arr::random($idmunicipios));
+    $title = array('Sr. ', 'Dr. ', 'Sra. ', 'Srta. ', 'Dr. ');
     return [
-        'nome' => $faker->name($sexo[$rand][1]),
-        'sexo'=> $sexo[$rand][0],
+        'nome' => str_replace($title, '', $faker->name($sexo[$rand][1])),
+        'sexo' => $sexo[$rand][0],
         'cpf' => $faker->cpf(false),
-        'nome_mae' => $faker->name('female'),
-        'nome_pai' => $faker->name('male'),
-        'data_nascimento' =>$faker->date('Y-m-d','2000-01-01'),
+        'nome_mae' => str_replace($title, '', $faker->name('female')),
+        'nome_pai' => str_replace($title, '', $faker->name('male')),
+        'data_nascimento' => $faker->date('Y-m-d', '2000-01-01'),
         'estado_civil' => Arr::random($estadoCivil),
         'tipo_documento' => Arr::random($tipoDocumento),
         'num_documento' => $faker->rg(false),
-        'data_emissao_documento' => $faker->date('Y-m-d','1998-01-01'),
+        'data_emissao_documento' => $faker->date('Y-m-d', '1998-01-01'),
         'orgao_emissor_documento' => 'SSP',
         'uf_documento' => $municipio->estado->id,
         'nascimento_municipio' => $municipio->id,
         'titulo_num' => $faker->randomNumber(8),
-        'titulo_emissao' => $faker->date('Y-m-d','2016-01-01'),
-        'titulo_municipio'=> $municipio->id
+        'titulo_emissao' => $faker->date('Y-m-d', '2016-01-01'),
+        'titulo_municipio' => $municipio->id
     ];
 });
