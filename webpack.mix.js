4698aa9ac1f41ba5f3ab0d96ea5f7078e6a7f3ab
Diogo Lindoso <diogolindoso@gmail.com>
2020-07-26 14:43:08 -0400

    atualizacao dos assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,12 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+   .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
    .sass('resources/sass/app.scss', 'public/css')
    .styles(['resources/css/multstep.css'],'public/css/all.css')
    .scripts([
       'resources/js/multstep.js',
       'resources/js/selectFilter.js',
-      'resources/js/multstepload.js'
    ],'public/js/all.js')
    .scripts(['resources/js/admin.js'],'public/js/adm.js')
    .copy(['node_modules/jquery-mask-plugin/dist/jquery.mask.js','node_modules/chart.js/dist/Chart.js'],'public/js')
